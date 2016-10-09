da415288ef643c14c5b98e6aff219470d88c1731
Ian Macalinao <me@ian.pw>
2016-10-09 14:07:00 -0500

    exchange client

---
+++ exchange/client.py
@@ -0,0 +1,24 @@
+import json
+
+def ExchangeClient(object):
+    """
+    Client for exchange.
+    """
+    def __init__(self):
+        pass
+
+    def handshake(self):
+        pass
+
+    def market(self, order_id, security, direction, quantity):
+        pass
+
+    def limit(self, order_id, security, direction, price, quantity):
+        pass
+
+    def cancel(self, order_id):
+        pass
+
+    def _handle_message(self, msg):
+        data = json.loads(msg)
+        # TODO(igm): handle message
